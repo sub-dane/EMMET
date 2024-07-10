1a6b89c86aa24eb30fc1bf852b18ed3c6dfa38f7
juespinosao <juespinosao@unal.edu.co>
2024-07-10 11:58:00 -0500

    Función de imputación

--- R/f3_imputacion.R
+++ R/f3_imputacion.R
@@ -49,23 +49,25 @@
 
 
 f3_imputacion <- function(directorio,mes,anio,avance=100) {
-
-
+  
+  
   # librerias ---------------------------------------------------------------
-
-
+  
+  
   library(dplyr)
   library(readxl)
   library(readr)
   library(data.table)
   library(VIM)
   library(forecast)
+  library(openxlsx)
+  library(imputeTS)
   source("https://raw.githubusercontent.com/NataliArteaga/DANE.EMMET/main/R/utils.R")
-
-
+  
+  
   #cargar base estandarizada
   datos <- read.csv(paste0(directorio,"/results/S1_integracion/EMMET_PANEL_trabajo_original_",meses[mes],anio,".csv"),fileEncoding = "latin1")
-
+  
   #crear una copia de la base de datos
   datoscom=datos
   #crear un data frame con las variables de interes
@@ -93,11 +95,11 @@ f3_imputacion <- function(directorio,mes,anio,avance=100) {
   #cargar la base de alertas
   if(avance==100){
     wowimp=fread(paste0(directorio,"/results/S2_identificacion_alertas/EMMET_PANEL_alertas_",meses[mes],anio,".csv"),encoding = "Latin-1")
-
-    }else{
- wowimp=fread(paste0(directorio,"/results/S2_identificacion_alertas/EMMET_PANEL_alertas_",meses[mes],anio,"_",avance,".csv"),encoding = "Latin-1")
-
-      }
+    
+  }else{
+    wowimp=fread(paste0(directorio,"/results/S2_identificacion_alertas/EMMET_PANEL_alertas_",meses[mes],anio,"_",avance,".csv"),encoding = "Latin-1")
+    
+  }
   wowimp=as.data.frame(wowimp)
   for (i in variablesinte) {
     wowimp[,i] <- as.numeric(wowimp[,i])
@@ -107,7 +109,7 @@ f3_imputacion <- function(directorio,mes,anio,avance=100) {
   for (i in variablesinte) {
     wowimp[!grepl("continua", tolower(wowimp[, paste0(i, "_caso_de_imputacion")])),i]<- NA
   }
-
+  
   wowimp=wowimp %>% select(ANIO,MES,NOVEDAD,DEPARTAMENTO,NOMBREMPIO,
                            NORDEST,NOMBRE_ESTABLECIMIENTO,DOMINIO_39,CLASE_CIIU4,
                            NPERS_EP,AJU_SUELD_EP,NPERS_ET,
@@ -121,41 +123,43 @@ f3_imputacion <- function(directorio,mes,anio,avance=100) {
                            AJU_VENTASEX,EXISTENCIAS)
   #Combinar la base sin el mes actual con la base de alertas
   base_imputar=rbind(datos,wowimp)
-
-
+  
+  
   # imputacion ---------------------------------------------------------------------
-
+  
   vec=c(0,0,0,0,1,1,0)
-
-
+  
+  
   #base_imputar=rbind(datos,wowimp)
   base_imputar2=base_imputar %>% filter((MES>=mes & ANIO==anio-2) |(ANIO==anio-1)|(MES<=mes & ANIO==anio))
-
-set.seed(11)
+  
+  set.seed(11)
   for (i in cap3) {
     ids=wowimp[which(!(complete.cases(wowimp[,paste0(i)]))),"NORDEST"]
     for (j in ids) {
-
+      
       mmm <- base_imputar2 %>% filter(NORDEST==j) %>%
         as.data.frame()
-
+      
       model_p <- auto.arima(as.ts(mmm[,i]))
       pru_mod=(model_p$arma==vec)
       if(sum(pru_mod)<7){
-      model <- model_p
+        model <- model_p
+        pred <- forecast(model, h = 1)$mean
       }else{
-      model <- arima(as.ts(mmm[,i]),c(0,0,0))
+        kalman <- na_kalman((mmm[,i]))
+        pred <-tail(kalman,1)
       }
-
-      pred <- forecast(model, h = 1)$mean
-
+      
+      
+      
       base_imputar2[base_imputar2$NORDEST==j & base_imputar2$ANIO==anio & base_imputar2$MES==mes,paste0(i)]=pred[1]
-
+      
     }
-
+    
   }
-
-
+  
+  
   arima_2anios=base_imputar2 %>% select(ANIO,MES,NOVEDAD,DEPARTAMENTO,NOMBREMPIO,
                                         NORDEST,NOMBRE_ESTABLECIMIENTO,DOMINIO_39,CLASE_CIIU4,
                                         NPERS_EP,AJU_SUELD_EP,NPERS_ET,
@@ -168,38 +172,38 @@ set.seed(11)
                                         AJU_PRODUCCION,AJU_VENTASIN,
                                         AJU_VENTASEX,EXISTENCIAS) %>% filter(MES==mes & ANIO==anio) %>%
     arrange(NORDEST)
-
-
-
+  
+  
+  
   for (i in cap3) {
     arima_2anios[,i]<- ifelse(arima_2anios[,i] < 0, 0, arima_2anios[,i])
   }
-
+  
   #crear una base con los valores del mes anterior
   tra <- base_imputar  %>% group_by(NORDEST) %>%
     mutate_at(c(variablesinte),.funs=list(rezago1=~lag(.)))#,
-
+  
   #convierte el NORDEST en factor
   tra$NORDEST <- as.factor(tra$NORDEST)
-
+  
   mes_ant <- tra  %>% filter(MES==mes & ANIO==anio)
-
+  
   mes_ant=as.data.frame(mes_ant)
-
-
+  
+  
   for (j in cap2) {
     #identificar las filas que tienen NA
     missing_values <- is.na(mes_ant[, j])
-
+    
     #imputar las variables del capítulo 2 por el valor del mes anterior
     mes_ant[missing_values, j] <- round(mes_ant[missing_values, paste0(j,"_rezago1")])
-
+    
   }
-
+  
   #filtrar solo por el mes de interés y dejar las variables importantes
   mes_ant=mes_ant%>% filter(MES==mes & ANIO==anio) %>%
     arrange(NORDEST)
-
+  
   mes_ant=mes_ant %>% select(ANIO,MES,NOVEDAD,DEPARTAMENTO,NOMBREMPIO,
                              NORDEST,NOMBRE_ESTABLECIMIENTO,DOMINIO_39,CLASE_CIIU4,
                              NPERS_EP,AJU_SUELD_EP,NPERS_ET,
@@ -211,27 +215,27 @@ set.seed(11)
                              AJU_HORAS_ORDI,AJU_HORAS_EXT,
                              AJU_PRODUCCION,AJU_VENTASIN,
                              AJU_VENTASEX,EXISTENCIAS,ends_with("_rezago1"))
-
-
+  
+  
   #quitar las columnas del capítulo 3 para imputarlos por el valor obtenido con los KNN
   mes_ant=arrange(mes_ant,mes_ant$NORDEST)
   mes_ant <- mes_ant[, !(names(mes_ant) %in% cap3)]
   mes_ant <- cbind(mes_ant, arima_2anios[, cap3])
-
+  
   for (i in cap3) {
-  mes_ant[,i]=ifelse(mes_ant[,paste0(i,"_rezago1")]==0,0,mes_ant[,i])
+    mes_ant[,i]=ifelse(mes_ant[,paste0(i,"_rezago1")]==0,0,mes_ant[,i])
   }
-
+  
   #crear una base con el mes de interes y ordenar por el NORDEST
   novtem=filter(datoscom, (ANIO == anio & MES == mes))
   novtem=arrange(novtem,novtem$NORDEST)
-
+  
   #convertir algunas variables en numericas
   novtem$NPERS_EP<-as.numeric(novtem$NPERS_EP)
   novtem$AJU_VENTASEX<-as.numeric(novtem$AJU_VENTASEX)
   novtem$AJU_HORAS_EXT<-as.numeric(novtem$AJU_HORAS_EXT)
   novtem=as.data.frame(novtem)
-
+  
   #Modificar la base copia para que no tenga el mes de interes
   datoscom <- filter(datoscom, !(ANIO == anio & MES == mes))
   datoscom<- as.data.frame(datoscom)
@@ -241,45 +245,45 @@ set.seed(11)
     datoscom[,i] <- as.numeric(datoscom[,i])
     datoscom[,i] <- ifelse(is.na(datoscom[,i]),0,datoscom[,i])
   }
-
+  
   #validar si hay 0 personas entonces el sueldo debe ser 0
   for (i in 1:8) {
     mes_ant[,paste0(personas[i],"_prueba")]=ifelse((mes_ant[,personas[i]]==0 &  mes_ant[,sueldos[i]]!=0) ,1,0)
     mes_ant[,paste0(sueldos[i],"_prueba")]=ifelse((mes_ant[,personas[i]]!=0 &  (mes_ant[,sueldos[i]]/mes_ant[,personas[i]])<1000) ,1,0)
-
+    
   }
-
-#arreglar las columnas que no cumplen ocn la regla
+  
+  #arreglar las columnas que no cumplen ocn la regla
   for (i in 1:8) {
-
- mes_ant[mes_ant[,paste0(personas[i],"_prueba")]==1,paste0(sueldos[i])]<- 0
-
+    
+    mes_ant[mes_ant[,paste0(personas[i],"_prueba")]==1,paste0(sueldos[i])]<- 0
+    
   }
-
-mes_ant[,"horasordi_prueba"]=ifelse((mes_ant$AJU_HORAS_ORDI/(24*(mes_ant$NPERS_OP+mes_ant$NPERS_OT+mes_ant$NPERS_OTA+mes_ant$NPERS_APREO))
+  
+  mes_ant[,"horasordi_prueba"]=ifelse((mes_ant$AJU_HORAS_ORDI/(24*(mes_ant$NPERS_OP+mes_ant$NPERS_OT+mes_ant$NPERS_OTA+mes_ant$NPERS_APREO))
   )<7 | (mes_ant$AJU_HORAS_ORDI/(24*(mes_ant$NPERS_OP+mes_ant$NPERS_OT+mes_ant$NPERS_OTA+mes_ant$NPERS_APREO))
   )>9,1,0)
-
-mes_ant[,"horasextras_prueba"]=ifelse(mes_ant$AJU_HORAS_EXT>mes_ant$AJU_HORAS_ORDI,1,0)
-
-mes_ant[,"existencias_prueba"]=ifelse((mes_ant[,"AJU_PRODUCCION"]>(mes_ant[,"AJU_VENTASEX"]+mes_ant[,"AJU_VENTASIN"]) & mes_ant[,"EXISTENCIAS"]<mes_ant[,"EXISTENCIAS_rezago1"])|(mes_ant[,"AJU_PRODUCCION"]<(mes_ant[,"AJU_VENTASEX"]+mes_ant[,"AJU_VENTASIN"]) & mes_ant[,"EXISTENCIAS"]>mes_ant[,"EXISTENCIAS_rezago1"]),1,0)
-
+  
+  mes_ant[,"horasextras_prueba"]=ifelse(mes_ant$AJU_HORAS_EXT>mes_ant$AJU_HORAS_ORDI,1,0)
+  
+  mes_ant[,"existencias_prueba"]=ifelse((mes_ant[,"AJU_PRODUCCION"]>(mes_ant[,"AJU_VENTASEX"]+mes_ant[,"AJU_VENTASIN"]) & mes_ant[,"EXISTENCIAS"]<mes_ant[,"EXISTENCIAS_rezago1"])|(mes_ant[,"AJU_PRODUCCION"]<(mes_ant[,"AJU_VENTASEX"]+mes_ant[,"AJU_VENTASIN"]) & mes_ant[,"EXISTENCIAS"]>mes_ant[,"EXISTENCIAS_rezago1"]),1,0)
+  
   #pegar las columnas imputadas en el data frame del mes actual
   for (i in variablesinte){
     novtem[,i]<- mes_ant[,i]
-
+    
   }
-
-
+  
+  
   #combinar las base original estandarizada con el mes imputado
-
+  
   imputa=rbind(datoscom,novtem)
   imputa=as.data.frame(imputa)
   for (i in variablesinte) {
     imputa[,i] <- as.numeric(imputa[,i])
   }
   imputa <- imputa %>% mutate_if(is.integer, as.numeric)
-
+  
   imputa <- imputa %>%
     mutate(
       TOTAL_VENTAS=(AJU_VENTASIN+AJU_VENTASEX),
@@ -288,11 +292,10 @@ mes_ant[,"existencias_prueba"]=ifelse((mes_ant[,"AJU_PRODUCCION"]>(mes_ant[,"AJU
                   NPERS_APREA+NPERS_OP+NPERS_OT+
                   NPERS_OTA+NPERS_APREO)
     )
-
+  
   # Exportar la base imputada -------------------------------
   write.csv(mes_ant,paste0(directorio,"/results/S3_imputacion/EMMET_reglas_consistencia_",meses[mes],anio,".csv"),row.names=F,fileEncoding = "latin1")
-
+  
   write.csv(imputa,paste0(directorio,"/results/S3_imputacion/EMMET_PANEL_imputada_",meses[mes],anio,".csv"),row.names=F,fileEncoding = "latin1")
 }
-
 
